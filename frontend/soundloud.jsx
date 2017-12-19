f5cd3057f1f978c95ff945b701a9f4a07c55a690
Edan Lewis <edan@lewisarts.com>
2017-12-19 18:30:10 -0500

    Updates entry file with store

--- frontend/soundloud.jsx
+++ frontend/soundloud.jsx
@@ -1,16 +1,12 @@
-import React from 'react';
+import Root from './components/root';
 import ReactDOM from 'react-dom';
-import { login, signup, logout } from './util/session_api_util';
 import configureStore from './store/store';
+import React from 'react';
 
 document.addEventListener('DOMContentLoaded', () => {
-  window.login = login;
-  window.signup = signup;
-  window.logout = logout;
-  
   const store = configureStore();
   window.getState = store.getState;
   window.dispatch = store.dispatch;
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome to SoundLoud</h1>, root);
+  ReactDOM.render(<Root store={ store } />, root);
 });
