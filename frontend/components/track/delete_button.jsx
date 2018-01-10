1e6e2dd92208078e0cb9c7c45d0de9c8b3e6cfb2
Edan Lewis <edan@lewisarts.com>
2018-01-09 16:31:32 -0800

    Search styling

--- frontend/components/track/delete_button.jsx
+++ frontend/components/track/delete_button.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 
 export default class DeleteButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(e) {
+        e.preventDefault();
+        this.props.destroyTrack();
+    }
     render() {
         const {track, destroyTrack} = this.props;
         return (
           
-            <button className="delete__button" onClick={destroyTrack}>
+            <button className="delete__button" onClick={this.handleClick}>
             <i className="fa fa-times" aria-hidden="true"></i>
             </button>
            
