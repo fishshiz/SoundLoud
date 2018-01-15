a70d19922b3f4ad2a5a50749b1393611cfc5b7ad
Edan Lewis <edan@lewisarts.com>
2018-01-14 21:40:04 -0800

    Init backend for comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180101211755) do
+ActiveRecord::Schema.define(version: 20180115053102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,16 @@ ActiveRecord::Schema.define(version: 20180101211755) do
     t.datetime "image_updated_at"
     t.index ["email"], name: "index_artists_on_email", unique: true
     t.index ["session_token"], name: "index_artists_on_session_token"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "author_id", null: false
+    t.integer "track_id", null: false
+    t.text "body", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_comments_on_author_id"
+    t.index ["track_id"], name: "index_comments_on_track_id"
   end
 
   create_table "tracks", force: :cascade do |t|
