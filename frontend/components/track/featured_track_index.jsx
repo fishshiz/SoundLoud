30221d8f1096719d442e4e9f9249ad5aa30335ec
Edan Lewis <edan@lewisarts.com>
2018-01-31 17:16:33 -0800

    Fixes lifecyle for featured page

--- frontend/components/track/featured_track_index.jsx
+++ frontend/components/track/featured_track_index.jsx
@@ -4,7 +4,6 @@ import FeaturedTrackItem from './featured_track_item';
 export default class FeaturedTrackIndex extends React.Component {
     constructor(props) {
         super(props);
-        console.log('constructor');
         let list;
         this.state = {
             list: 'featured'
@@ -13,59 +12,41 @@ export default class FeaturedTrackIndex extends React.Component {
     }
     
     componentDidMount() {
-        console.log('component mounted');
         if(this.state.list === 'featured') this.props.requestFeaturedTracks();
         if(this.state.list === 'discussed') this.props.requestDiscussedTracks();
-        console.log(this.state);
     }
     
     componentWillUnmount() {
-        console.log('component unmounted');
         this.props.removeMainTracks();
     }
-
-    // componentDidUpdate() {
-    //     if(this.state.featured) this.props.requestFeaturedTracks();
-    //     if(this.state.discussed) this.props.requestDiscussedTracks();
-    //     console.log(this.state);
-    // }
-
-    // shouldComponentUpdate() {
-
-    //     if (this.state.list !== 'featured') return true;
-    //     return true;
-    // }
     
    toggleList(e) {
-       console.log('toggle list');
-       e.preventDefault();
-       let list;
-    //    debugger;
-       if (this.state.list === 'featured') {
-           list = 'discussed';
-       } else {
-           list = 'featured';
-       }
+        e.preventDefault();
+        let element = e.currentTarget;
+        let other = Array.from(element.classList).includes('featured') ? document.querySelector('.discussed') : document.querySelector('.featured');
+        other.classList.remove('selected');
+        element.classList.add('selected');
+        let list;
+        if (this.state.list === 'featured') {
+            this.props.requestDiscussedTracks();
+            list = 'discussed';
+        } else {
+            this.props.requestFeaturedTracks();
+            list = 'featured';
+        }
         this.setState({
             list
-        });
-        // debugger;
-        if(this.state.list === 'featured') this.props.requestFeaturedTracks();
-        if(this.state.list === 'discussed') this.props.requestDiscussedTracks();
-        console.log('state', this.state);
-        
+        });        
     }
     
     render() {
-        // debugger;
-        console.log('render');
         let subheading = this.state.list === 'featured' ? 'Hear Soundloud\'s most popular tracks:' : 'Hear Soundloud\'s most discussed tracks:';
                 return (
                     <div className="l-container l-content">
                         <div>
                             <div className="l-fluid-fixed">
                                 <div className="featured__title">
-                                    <h2 className="featured" onClick={this.toggleList}>Featured Tracks</h2>
+                                    <h2 className="featured selected" onClick={this.toggleList}>Featured Tracks</h2>
                                     <h2 className="discussed" onClick={this.toggleList}>Most Discussed Tracks</h2>
                                 </div>
                                 <div className="stream__header g-flex-row-centered-spread">
