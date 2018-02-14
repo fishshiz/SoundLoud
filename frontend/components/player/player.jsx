53971d25b111847c2020c5f1bbf6fafbbc5591fc
Edan Lewis <edan@lewisarts.com>
2018-02-14 13:01:19 -0800

    Introduces hand rolled audio player

--- frontend/components/player/player.jsx
+++ frontend/components/player/player.jsx
@@ -6,11 +6,15 @@ export default class Player extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      track: { id: "", title: "", imageUrl: "", audio_url: "" }
+      track: { id: "", title: "", imageUrl: "", audio_url: "" },
+      volume: 1
     };
+
     this.grabArtistName = this.grabArtistName.bind(this);
     this.updatePlayCount = this.updatePlayCount.bind(this);
     this.handleKeyDown = this.handleKeyDown.bind(this);
+    this.changeVolume = this.changeVolume.bind(this);
+    // this.scrub = this.scrub.bind(this);
   }
 
   componentDidMount(track) {
@@ -33,12 +37,26 @@ export default class Player extends React.Component {
     }
   }
 
+  changeVolume() {
+    const player = document.querySelector(".player");
+    const audio = player.querySelector(".html__player");
+    const toggle = player.querySelector(".toggle");
+    const volumee = player.querySelector(".player__slider");
+    audio.volume = volumee.value;
+  }
+
   togglePlayPause() {
-    const { audioEl } = this.rap;
-    if (audioEl.paused) {
-      audioEl.play();
+    const player = document.querySelector(".player");
+    const audio = player.querySelector(".html__player");
+    const toggle = player.querySelector(".toggle");
+    const volume = player.querySelector(".player__slider");
+
+    if (audio.paused) {
+      audio.play();
+      toggle.textContent = "❚ ❚";
     } else {
-      audioEl.pause();
+      audio.pause();
+      toggle.textContent = "►";
     }
   }
 
@@ -53,18 +71,31 @@ export default class Player extends React.Component {
   }
 
   componentDidMount() {
-    this.rap.audioEl.setAttribute("controlsList", "nodownload");
+    // this.rap.audioEl.setAttribute("controlsList", "nodownload");
     const body = document.getElementById("body");
     body.addEventListener("keydown", this.handleKeyDown);
+    const player = document.querySelector(".player");
+    const audio = player.querySelector(".html__player"); 
+    audio.addEventListener('timeupdate', this.handleProgress.bind(this));
+  }
+
+  scrub(e) {
+    e.preventDefault();
+    const player = document.querySelector(".player");
+    const audio = player.querySelector(".html__player");
+    const progress = player.querySelector(".progress");
+    const scrubTime =
+      e.nativeEvent.offsetX / progress.offsetWidth * audio.duration;
+    audio.currentTime = scrubTime;
   }
 
   componentWillReceiveProps({ track, paused }) {
-    const { audioEl } = this.rap;
-    if (paused && !audioEl.paused) {
-      audioEl.pause();
-    } else if (!paused && audioEl.paused) {
-      audioEl.play();
-    }
+    // const { audioEl } = this.rap;
+    // if (paused && !audioEl.paused) {
+    //   audioEl.pause();
+    // } else if (!paused && audioEl.paused) {
+    //   audioEl.play();
+    // }
 
     this.setState({ track });
   }
@@ -83,9 +114,20 @@ export default class Player extends React.Component {
     }
   }
 
+  handleProgress() {
+    const player = document.querySelector(".player");
+    const audio = player.querySelector(".html__player");
+    const progressBar = player.querySelector(".progress__filled");
+
+  const percent = (audio.currentTime / audio.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
   render() {
     const { track } = this.state;
     let image = null;
+    let title = null;
+    let artist = null;
     if (track.image_url) {
       image = (
         <Link to={`/tracks/${track.id}`}>
@@ -95,38 +137,48 @@ export default class Player extends React.Component {
           />
         </Link>
       );
+      artist = this.props.artists[this.state.track.artist_id].name;
+
     } else {
       image = <div className="playbackSoundBadge__avatar sc-media-image" />;
     }
 
     return (
-      <div className="playControls g-z-index-header m-visible">
-        <div className="playControls__inner">
-          <div className="playControls__wrapper l-container l-fullwidth">
-            <ReactAudioPlayer
-              className="player"
-              src={track.audio_url}
-              style={{
-                display: "flex",
-                alignItems: "center",
-                backgroundColor: "orange",
-                color: "orange"
-              }}
-              ref={element => {
-                this.rap = element;
-              }}
-              autoPlay
-              controls
-              onPlay={this.updatePlayCount}
-            />
-            <div className="playbackSoundBadge">
-              {image}
-              <div className="playbackSoundBadge__titleContextContainer">
-                {this.grabArtistName()}
-                <div className="playbackSoundBadge__title">
-                  <div className="playbackSoundBadge__titleLink sc-truncate">
-                    <Link to={`/tracks/${track.id}`}>{track.title}</Link>
-                  </div>
+      <div className="player">
+        <audio className="html__player" src={track.audio_url} />
+        <div className="player__controls">
+          <div className="progress" onClick={this.scrub.bind(this)}>
+            <div className="progress__filled" />
+          </div>
+          <button
+            className="player__button toggle"
+            title="Toggle Play"
+            onClick={this.togglePlayPause}
+          >
+            ►
+          </button>
+          <input
+            type="range"
+            name="volume"
+            className="player__slider"
+            min="0"
+            max="1"
+            step="0.05"
+            onChange={this.changeVolume}
+          />
+          <button data-skip="-10" className="player__button">
+            «
+          </button>
+          <button data-skip="25" className="player__button">
+            »
+          </button>
+          <div className="playbackSoundBadge">
+            {image}
+            <div className="playbackSoundBadge__titleContextContainer">
+              {artist}
+              <div className="playbackSoundBadge__title">
+                <div className="playbackSoundBadge__titleLink sc-truncate">
+                  <Link to={`/tracks/${track.id}`}>{track.title}</Link>
                 </div>
               </div>
             </div>
