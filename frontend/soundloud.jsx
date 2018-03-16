1d74d6ac20de264a307f33bb138dd1c5d7ab48b6
Edan Lewis <edan@lewisarts.com>
2018-03-15 17:18:44 -0700

    Code cleanup

--- frontend/soundloud.jsx
+++ frontend/soundloud.jsx
@@ -2,7 +2,6 @@ import Root from "./components/root";
 import ReactDOM from "react-dom";
 import configureStore from "./store/store";
 import React from "react";
-import { fetchTrackComments } from "./util/comment_api_util";
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
@@ -11,7 +10,6 @@ document.addEventListener("DOMContentLoaded", () => {
   if (window.currentArtist) {
     preloadedState = { session: { currentArtist: window.currentArtist } };
   }
-  window.fetchTrackComments = fetchTrackComments;
   const store = configureStore(preloadedState);
   window.getState = store.getState;
   window.dispatch = store.dispatch;
